b380d4c94d5d6643c8ef765db825f534e17c1f88
dan <37590801+dan-auth0@users.noreply.github.com>
2023-12-07 15:58:18 -0500

    add prettier and remove inconsistent eslint config

--- public/components/step-one.js
+++ public/components/step-one.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import offset from 'document-offset';
-import { validateUrl } from '../utils';
+import React from "react";
+import offset from "document-offset";
+import { validateUrl } from "../utils";
 
 class StepOne extends React.Component {
-
   constructor() {
     super();
 
@@ -11,49 +10,49 @@ class StepOne extends React.Component {
     this.update = this.update.bind(this);
     this.returnFromAuth = this.returnFromAuth.bind(this);
 
-    let savedState = localStorage.getItem('app-state') || '{}'
-    savedState = JSON.parse(savedState)
+    let savedState = localStorage.getItem("app-state") || "{}";
+    savedState = JSON.parse(savedState);
 
-    this.state = savedState
-    this.state.stepState = 'initial'
-    this.state.isActiveStep = (this.state.currentStep === 1)
+    this.state = savedState;
+    this.state.stepState = "initial";
+    this.state.isActiveStep = this.state.currentStep === 1;
   }
 
-  update(){
+  update() {
     this.forceUpdate();
   }
 
   start() {
-    if(!this.props.authEndpoint) {
-      return alert('Plase enter an authorization endpoint');
+    if (!this.props.authEndpoint) {
+      return alert("Plase enter an authorization endpoint");
     }
 
     this.setState({
-      stepState: 'wait'
+      stepState: "wait",
     });
 
     if (!validateUrl(this.completeURL)) {
-      alert('You entered an invalid server address');
+      alert("You entered an invalid server address");
       return window.location.reload();
     }
 
-    window.location = this.completeURL
+    window.location = this.completeURL;
   }
 
   returnFromAuth() {
     let c = this;
 
     c.setState({
-      stepState: 'wait',
-      isActiveStep: c.props.isActive
+      stepState: "wait",
+      isActiveStep: c.props.isActive,
     });
 
-    setTimeout(function() {
-      let step2 = document.getElementById('step2');
+    setTimeout(function () {
+      let step2 = document.getElementById("step2");
 
       c.setState({
-        stepState: 'initial',
-        isActiveStep: false
+        stepState: "initial",
+        isActiveStep: false,
       });
 
       c.props.scrollAnimated(offset(step2).top - 30, 600);
@@ -63,80 +62,122 @@ class StepOne extends React.Component {
   componentDidMount() {
     var c = this;
 
-    window.addEventListener('returnFromAuth', function() {
-      c.returnFromAuth()
+    window.addEventListener("returnFromAuth", function () {
+      c.returnFromAuth();
     });
 
-    window.addEventListener('startOver', function() {
+    window.addEventListener("startOver", function () {
       c.setState({
-        isActiveStep: true
+        isActiveStep: true,
       });
     });
   }
 
   render() {
-    this.completeURL = this.props.authEndpoint + '?client_id=' + this.props.clientID + '&redirect_uri=' + this.props.redirectURI +'&scope=' + encodeURI(this.props.scopes) + '&response_type=code&state=' + this.props.stateToken;
+    this.completeURL =
+      this.props.authEndpoint +
+      "?client_id=" +
+      this.props.clientID +
+      "&redirect_uri=" +
+      this.props.redirectURI +
+      "&scope=" +
+      encodeURI(this.props.scopes) +
+      "&response_type=code&state=" +
+      this.props.stateToken;
 
-    if(this.props.audience && this.props.audience !== "") {
+    if (this.props.audience && this.props.audience !== "") {
       this.completeURL += `&audience=${this.props.audience}`;
     }
 
     return (
-      <div className={`playground-step ${this.state.isActiveStep ? 'active' : '' }`}>
+      <div
+        className={`playground-step ${this.state.isActiveStep ? "active" : ""}`}
+      >
         <span className="step-number">1</span>
         <div className="step-content">
           <h2 className="step-title">Redirect to OpenID Connect Server</h2>
           <div className="code-box">
             <h3 className="code-box-title">
               Request
-              { this.state.showResponse === 'response' ? ' / Response' : null }
+              {this.state.showResponse === "response" ? " / Response" : null}
             </h3>
             <div className="code-box-content">
               <div className="code-block">
-                <a onClick={() => { this.props.openModal(true, 'authEndpoint') }} href="#"> { this.props.authEndpoint || "Enter an authorization endpoint"}? </a>
+                <a
+                  onClick={() => {
+                    this.props.openModal(true, "authEndpoint");
+                  }}
+                  href="#"
+                >
+                  {" "}
+                  {this.props.authEndpoint || "Enter an authorization endpoint"}
+                  ?{" "}
+                </a>
                 <div className="code-block-url-params">
                   client_id=
-                  <a onClick={() => { this.props.openModal(true, 'clientID') }} href="#">{this.props.clientID}</a>
+                  <a
+                    onClick={() => {
+                      this.props.openModal(true, "clientID");
+                    }}
+                    href="#"
+                  >
+                    {this.props.clientID}
+                  </a>
                   <br />
-                  &amp;redirect_uri=
-                  https://openidconnect.net/callback
+                  &amp;redirect_uri= https://openidconnect.net/callback
                   <br />
                   &amp;scope=
-                  <a onClick={() => { this.props.openModal(true, 'scopes') }} href="#">{this.props.scopes}</a>
-                  <br/>
+                  <a
+                    onClick={() => {
+                      this.props.openModal(true, "scopes");
+                    }}
+                    href="#"
+                  >
+                    {this.props.scopes}
+                  </a>
+                  <br />
                   <span>&amp;response_type=code</span>
                   <br />
                   <span>&amp;state={this.props.stateToken}</span>
-
-                  {this.props.audience&& <br/>}
-                  {this.props.audience&& '&audience='}
-                  {this.props.audience&& <a onClick={() => { this.props.openModal(true, 'audience') }} href="#">{this.props.audience}</a>}
+                  {this.props.audience && <br />}
+                  {this.props.audience && "&audience="}
+                  {this.props.audience && (
+                    <a
+                      onClick={() => {
+                        this.props.openModal(true, "audience");
+                      }}
+                      href="#"
+                    >
+                      {this.props.audience}
+                    </a>
+                  )}
                 </div>
               </div>
               <hr />
-              { this.state.stepState === 'wait' ?
+              {this.state.stepState === "wait" ? (
                 <div className="theme-dark step-spinner-container">
                   <div className="spinner spinner-md step-spinner">
                     <div className="circle"></div>
                   </div>
                 </div>
-                : null }
-              { this.state.stepState === 'response' ?
+              ) : null}
+              {this.state.stepState === "response" ? (
                 <div className="code-block">
                   https://openidconnect.net/callback?code=#4/SXjuF3gzD04Oouq
                 </div>
-                : null
-              }
-              { this.state.stepState !== 'wait' ?
-                <button onClick={this.start} className="code-box-btn">Start</button>
-              : null }
+              ) : null}
+              {this.state.stepState !== "wait" ? (
+                <button onClick={this.start} className="code-box-btn">
+                  Start
+                </button>
+              ) : null}
             </div>
           </div>
           <br />
         </div>
         <button
           onClick={this.props.skipTutorial}
-          style={this.state.isActiveStep ? {} : {visibility: 'hidden'}}
+          style={this.state.isActiveStep ? {} : { visibility: "hidden" }}
           className="skip-tutorial btn-link"
         >
           Skip this tutorial. Show me the complete flow.
