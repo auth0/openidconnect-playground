101d0cc58d2ce215dc708c1285e71acea2d5703d
Byron Motoche <116190812+byron-okta@users.noreply.github.com>
2024-11-05 15:08:05 -0500

    fix: update webpack and nodemon dependencies that rely on a deprecated version of fsevents

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,38 @@
 module.exports = {
-    entry: "./public/app.js",
-    output: {
-        path: __dirname + "/public",
-        filename: "index.js",
-    },
-    module: {
-        rules: [{
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                query: {
-                    presets: ["@babel/preset-env", "@babel/preset-react"],
-                },
+  entry: "./public/app.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: "asset/inline",
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "stylus-loader",
+            options: {
+              stylusOptions: {
+                resolveURL: true,
+              },
             },
-            {
-                test: /\.(png|jpg|gif|svg)$/i,
-                use: [{
-                    loader: 'url-loader',
-                }],
-            }, {
-                test: /\.styl$/,
-                loader: "style-loader!css-loader!stylus-loader?resolve url",
-            },
+          },
         ],
-    },
+      },
+    ],
+  },
 };
