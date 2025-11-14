e269d976a6f211e4117ffa4b2eea7905628ed892
Javier Tinoco <213990346+javiert-okta@users.noreply.github.com>
2025-11-14 10:00:17 -0500

    format files

--- api/auth_data.js
+++ api/auth_data.js
@@ -1,34 +1,31 @@
 const crypto = require("crypto");
-const cookie = require("cookie");
-const signature = require("cookie-signature");
+const cookie = require("cookie")
+const signature = require("cookie-signature")
 
 export default function handler(req, res) {
-  const secret = process.env.JWT_SECRET;
-  const cookies = cookie.parse(req.headers.cookie || "");
-  const refresh = cookies.refresh?.startsWith("s:")
-    ? signature.unsign(cookies.refresh.slice(2), secret)
-    : null;
-  const authCode = cookies.authCode?.startsWith("s:")
-    ? signature.unsign(cookies.authCode.slice(2), secret)
-    : null;
-  let code = null;
-  if (refresh === "false" && authCode) {
-    code = authCode;
-    res.setHeader("Set-Cookie", [
-      cookie.serialize("refresh", "true", {
-        httpOnly: true,
-        secure: true,
-        path: "/",
-        sameSite: "Lax",
-      }),
-    ]);
-  }
-  const state = crypto.randomBytes(20).toString("hex");
-  res.status(200).json({
-    redirect_uri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    state,
-    code,
-  });
+    const secret = process.env.JWT_SECRET
+    const cookies = cookie.parse(req.headers.cookie || '')
+    const refresh = cookies.refresh?.startsWith('s:') ? signature.unsign(cookies.refresh.slice(2), secret) : null
+    const authCode = cookies.authCode?.startsWith('s:') ? signature.unsign(cookies.authCode.slice(2), secret) : null
+    let code = null;
+    if (refresh === "false" && authCode) {
+        code = authCode;
+        res.setHeader("Set-Cookie", [
+            cookie.serialize("refresh", "true", {
+                httpOnly: true,
+                secure: true,
+                path: "/",
+                sameSite: "Lax"
+            })
+        ]);
+    }
+    const state = crypto.randomBytes(20).toString("hex")
+    res.status(200).json({
+        redirect_uri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        state,
+        code
+    })
+
 }
--- api/callback.js
+++ api/callback.js
@@ -1,33 +1,33 @@
-const cookie = require("cookie");
-const signature = require("cookie-signature");
+const cookie = require('cookie')
+const signature = require('cookie-signature')
 
-export default function handler(req, res) {
-  if (req.method === "GET") {
-    const { code } = req.query;
-    if (code) {
-      const secret = process.env.JWT_SECRET;
-      const signedRefresh = "s:" + signature.sign("false", secret);
-      const signedAuthCode = "s:" + signature.sign(code, secret);
-      res.setHeader("Set-Cookie", [
-        cookie.serialize("authCode", signedAuthCode, {
-          httpOnly: true,
-          secure: true,
-          path: "/",
-          sameSite: "Lax",
-        }),
-        cookie.serialize("refresh", signedRefresh, {
-          httpOnly: true,
-          secure: true,
-          path: "/",
-          sameSite: "Lax",
-        }),
-      ]);
-      res.writeHead(302, { Location: "/" });
-      res.end();
+export default function handler(req,res) {
+    if(req.method === "GET") {
+        const { code } = req.query
+        if(code) {
+            const secret = process.env.JWT_SECRET
+            const signedRefresh = 's:' + signature.sign('false', secret)
+            const signedAuthCode = 's:' + signature.sign(code, secret) 
+            res.setHeader("Set-Cookie", [
+                cookie.serialize("authCode", signedAuthCode, {
+                    httpOnly: true,
+                    secure: true,
+                    path: "/",
+                    sameSite: "Lax"
+                }),
+                cookie.serialize("refresh", signedRefresh, {
+                    httpOnly: true,
+                    secure: true,
+                    path: "/",
+                    sameSite: "Lax"
+                })
+            ]);
+            res.writeHead(302, { Location: "/"})
+            res.end()
+        } else {
+            res.status(400).send("Missing Code")
+        }
     } else {
-      res.status(400).send("Missing Code");
+        res.status(405).send("Method not allowed")
     }
-  } else {
-    res.status(405).send("Method not allowed");
-  }
 }
