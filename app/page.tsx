db749aab439f7a030adc5f869e729c455bf98e5e
Javier Tinoco <213990346+javiert-okta@users.noreply.github.com>
2026-01-05 12:18:14 -0500

    render hero jumbotron on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { HeroJumbotronComponent } from "features/common/components/hero-jumbotron/hero-jumbotron.component";
+
 export default function Page() {
-    return <></>
+    return <HeroJumbotronComponent />
 }
