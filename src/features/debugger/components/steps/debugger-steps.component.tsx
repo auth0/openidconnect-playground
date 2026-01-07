75a1f96045e89a140a90b34c436d57da284a08b3
Javier Tinoco <213990346+javiert-okta@users.noreply.github.com>
2026-01-07 16:56:14 -0500

    add debugger steps component

---
+++ src/features/debugger/components/steps/debugger-steps.component.tsx
@@ -0,0 +1,68 @@
+"use client"
+import { ComponentType, useState } from "react";
+import styles from "./debugger-steps.module.scss";
+import { StepOne, StepTwo, StepThree, StepFour } from "./index";
+
+type Steps = {
+  id: string;
+  label: string;
+  component: ComponentType;
+};
+const stepsList: Steps[] = [
+  {
+    id: "step-one",
+    label: "Redirect to OpenID Connect Server",
+    component: StepOne,
+  },
+  {
+    id: "step-two",
+    label: "Exchange Code from Token",
+    component: StepTwo,
+  },
+  {
+    id: "step-three",
+    label: "Verify User Token",
+    component: StepThree,
+  },
+  {
+    id: "step-four",
+    label: "The token is valid!",
+    component: StepFour,
+  },
+];
+
+export const DebuggerSteps = () => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          {stepsList.map((step, index) => {
+            const state =
+              index < currentStepIndex
+                ? "completed"
+                : index === currentStepIndex
+                ? "current"
+                : "upcoming";
+            return (
+              <div
+                key={step.id}
+                className={styles.step_container}
+                data-state={state}
+                aria-current={state === "current" ? "step" : undefined}
+                aria-label={`${step.label} ${state}`}
+              >
+                <div className={styles.step_content}>
+                  {" "}
+                  <div className={styles.step_number}>{index + 1}</div>
+                  <p>{step.label}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
