6d8ee46bd364219ca917c15a1b3156631ab86126
Javier Tinoco <213990346+javiert-okta@users.noreply.github.com>
2026-01-14 17:01:54 -0500

    fix codeblock styling

--- src/features/debugger/components/codeblock/codeblock.component.tsx
+++ src/features/debugger/components/codeblock/codeblock.component.tsx
@@ -17,31 +17,33 @@ interface CodeBlockProps {
 export const Codeblock = (props: CodeBlockProps) => {
   const { title, type, requestData } = props;
   return (
-    <div className={styles.container}>
-      <div className={styles.title_container}>{title}</div>
-      <div className={styles.code_block}>
-        {type === "request" && requestData ? (
-          <>
-            <div className={styles.code_line}>
-              <p>01</p>
-              <p className={styles.param_value} data-editable={"true"}>
-                <span>{`${requestData.url}?`}</span>
-              </p>
-            </div>
-            {requestData.params.map((data, idx) => (
-              <div key={idx} className={styles.code_line}>
-                <p>{`0${idx + 2}`}</p>
-                <p
-                  className={styles.param_value}
-                  data-editable={data.isEditable ? "true" : "false"}
-                >
-                  {`${idx > 0 ? "&" : ""}${data.key}=`}
-                  <span>{data.value}</span>
+    <div className={styles.scroll_container}>
+      <div className={styles.container}>
+        <div className={styles.title_container}>{title}</div>
+        <div className={styles.code_block}>
+          {type === "request" && requestData ? (
+            <>
+              <div className={styles.code_line}>
+                <p className={styles.code_line_number}>01</p>
+                <p className={styles.param_value} data-editable={"true"}>
+                  <span>{`${requestData.url}?`}</span>
                 </p>
               </div>
-            ))}
-          </>
-        ) : null}
+              {requestData.params.map((data, idx) => (
+                <div key={idx} className={styles.code_line}>
+                  <p className={styles.code_line_number}>{`0${idx + 2}`}</p>
+                  <p
+                    className={styles.param_value}
+                    data-editable={data.isEditable ? "true" : "false"}
+                  >
+                    {`${idx > 0 ? "&" : ""}${data.key}=`}
+                    <span>{data.value}</span>
+                  </p>
+                </div>
+              ))}
+            </>
+          ) : null}
+        </div>
       </div>
     </div>
   );
