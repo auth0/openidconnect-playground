700cad439b3d5c5117a7be56c522dc9c38b7f63e
Javier Tinoco <213990346+javiert-okta@users.noreply.github.com>
2026-01-16 11:17:31 -0500

    add step two content and styling

--- src/features/debugger/components/steps/step-two/step-two.component.tsx
+++ src/features/debugger/components/steps/step-two/step-two.component.tsx
@@ -1,3 +1,48 @@
+import { Button } from "features/common/components/button/button.component";
+import { Codeblock, RequestData } from "../../codeblock/codeblock.component";
+import styles from "./step-two.module.scss";
+
+//TODO: replace with actual data coming from local state and api endpoint
+const REQUEST_DATA: RequestData = {
+  url: "https://samples.auth0.com/oauth/token",
+  method: "POST",
+  isEditable: false,
+  params: [
+    {
+      key: "grant_type",
+      value: "authorization_code",
+    },
+    {
+      key: "client_id",
+      value: "testvalue1234456677888",
+      isEditable: true,
+    },
+    {
+      key: "client_secret",
+      value: "testvalue1234456677888",
+      isEditable: true,
+    },
+    {
+      key: "redirect_uri",
+      value: "https://openidconnect.net/callback",
+    },
+    {
+      key: "code",
+      value: "test123456789",
+    },
+  ],
+};
+
 export const StepTwo = () => {
-    return <p>Step Two</p>
-}
+  return (
+    <>
+      <Codeblock title="Your Code" type="token" token="asdfasdfasdfsadfsdf" />
+      <p className={styles.description}>
+        Now, we need to turn that access code into an access token, by having
+        our server make a request to your token endpoint
+      </p>
+      <Codeblock title="Request" type="request" requestData={REQUEST_DATA} />
+      <Button label="Start" />
+    </>
+  );
+};
