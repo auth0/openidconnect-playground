b3dcb27167ad8c910bd390335c0b25aca52bec26
Javier Tinoco <213990346+javiert-okta@users.noreply.github.com>
2026-01-22 12:28:30 -0500

    add link button component

--- src/features/common/components/button/button.component.tsx
+++ src/features/common/components/button/button.component.tsx
@@ -2,29 +2,68 @@ import { ArrowIcon } from "features/common/icons/arrow.icon";
 import styles from "./button.module.scss";
 import clsx from "clsx";
 
+type BaseButtonProps = {
+  label: string;
+};
+
+type CommonButtonProps = {
+  variant?: "default" | "transparent";
+} & BaseButtonProps;
+
+const ButtonBase = ({ label }: BaseButtonProps) => {
+  return <span>{label}</span>;
+};
+
+type ButtonProps = {
+  onClick?: () => void;
+} & CommonButtonProps;
+
 export const Button = ({
   label,
   onClick,
-  variant="normal",
-}: {
-  label: string;
-  onClick?: () => void;
-  variant?: "normal" | "transparent";
-}) => {
+  variant = "default",
+}: ButtonProps) => {
   return (
     <button
       className={clsx(
         styles.button,
-        variant === "normal"
-          ? styles.button_variant_normal
-          : styles.button_variant_transparent
+        variant === "default"
+          ? styles.button_variant_default
+          : styles.button_variant_transparent,
       )}
       onClick={onClick}
     >
-      {label}
+      <ButtonBase label={label} />
       <div className={styles.button_arrow}>
         <ArrowIcon />
       </div>
     </button>
+  );
+};
+
+type LinkButtonProps = {
+  href: string;
+} & CommonButtonProps;
+
+export const LinkButton = ({
+  label,
+  href,
+  variant = "default",
+}: LinkButtonProps) => {
+  return (
+    <a
+      className={clsx(
+        styles.button,
+        variant === "default"
+          ? styles.button_variant_default
+          : styles.button_variant_transparent,
+      )}
+      href={href}
+    >
+      <ButtonBase label={label} />
+      <div className={styles.button_arrow}>
+        <ArrowIcon />
+      </div>
+    </a>
   );
 };
