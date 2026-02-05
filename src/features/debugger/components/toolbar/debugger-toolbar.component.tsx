db60eebede92c035b38c806c94c13dc47c6aa11c
Javier Tinoco <213990346+javiert-okta@users.noreply.github.com>
2026-02-05 16:47:35 -0500

    add modal to debugger steps component

--- src/features/debugger/components/toolbar/debugger-toolbar.component.tsx
+++ src/features/debugger/components/toolbar/debugger-toolbar.component.tsx
@@ -2,27 +2,40 @@
 import { DebuggerPickerComponent } from "features/common/components/debugger-picker/debugger-picker.component";
 import styles from "./debugger-toolbar.module.scss";
 import ConfigurationIcon from "features/common/icons/configuration-icon";
+import { useState } from "react";
+import { ConfigurationModal } from "../configuration-modal/configuration-modal.component";
 
 export const DebuggerToolbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className={styles.toolbar}>
-      <div className={styles.toolbarWrapper}>
-        <div className={styles.toolbarContent}>
-          <p className={styles.toolbarTitle}>Debugger</p>
-          <div className={styles.toolbarOptionsContainer}>
-            <DebuggerPickerComponent
-              label="Mode"
-              options={[{ value: "OAuth2", label: "OpenID Connect + OAuth2" }]}
-              minWidth={"180px"}
-            />
-            <div className={styles.toolbarSeparatorLine} />
-            <button className={styles.toolbarButtonContainer}>
-              <ConfigurationIcon />
-              <p>Configuration</p>
-            </button>
+    <>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.content}>
+            <p className={styles.title}>Debugger</p>
+            <div className={styles.container_options}>
+              <DebuggerPickerComponent
+                label="Mode"
+                options={[
+                  { value: "OAuth2", label: "OpenID Connect + OAuth2" },
+                ]}
+                minWidth={"180px"}
+                placeholder=""
+              />
+              <div className={styles.separator_line} />
+              <div
+                className={styles.button_container}
+                onClick={() => setIsOpen(true)}
+              >
+                <ConfigurationIcon />
+                <p>Configuration</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <ConfigurationModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
+    </>
   );
 };
