58de5ad714158fa4a9b8feaa0ff77df807800e27
Javier Tinoco <213990346+javiert-okta@users.noreply.github.com>
2026-02-13 16:41:31 -0500

    add logic to fetch discovery url, save form data changes

--- src/features/debugger/components/configuration-modal/configuration-modal.component.tsx
+++ src/features/debugger/components/configuration-modal/configuration-modal.component.tsx
@@ -2,64 +2,177 @@ import { AlertIcon } from "features/common/icons/alert.icon";
 import { CloseCircleIcon } from "features/common/icons/close-circle.icon";
 import styles from "./configuration-modal.module.scss";
 import { Button } from "features/common/components/button/button.component";
+import { useState } from "react";
+
+export type InitialModalData = {
+  domain: string;
+  serverTemplate: string;
+  authEndpoint: string;
+  tokenEndpoint: string;
+  clientId: string;
+  clientSecret: string;
+  scope: string;
+  audience: string;
+};
 
 type ModalOptions = {
+  name: string;
   title: string;
   defaultValue: string | null;
   type: "input" | "select";
-  isEditable?: boolean;
+  options?: { label: string; value: string }[];
+  readOnly?: boolean;
   optional?: boolean;
 };
 
 const MODAL_OPTIONS: ModalOptions[] = [
   {
+    name: "serverTemplate",
     title: "Server template",
-    defaultValue: "Placeholder",
-    type: "input",
+    defaultValue: "auth0",
+    type: "select",
+    options: [
+      {
+        label: "Auth0",
+        value: "auth0",
+      },
+      {
+        label: "Google",
+        value: "google",
+      },
+      {
+        label: "Custom",
+        value: "custom",
+      },
+    ],
   },
   {
-    title: "Auth0 domain",
+    name: "domain",
+    title: "Domain",
     defaultValue: "sample.auth0.com",
     type: "input",
   },
   {
+    name: "authEndpoint",
     title: "Endpoint",
     defaultValue: "sample.auth0.com/authorize",
     type: "input",
-    isEditable: false,
+    readOnly: true,
   },
   {
+    name: "tokenEndpoint",
     title: "Token Endpoint",
     defaultValue: "sample.auth0.com/oauth/token",
     type: "input",
-    isEditable: false,
+    readOnly: true,
   },
   {
+    name: "clientId",
     title: "OIDC Client ID",
-    defaultValue: "Placeholder",
+    defaultValue: "",
     type: "input",
   },
   {
-    title: "Title",
-    defaultValue: "Placeholder",
+    name: "clientSecret",
+    title: "OIDC Client Secret",
+    defaultValue: "",
     type: "input",
   },
   {
+    name: "scope",
     title: "Scope",
-    defaultValue: "Placeholder",
+    defaultValue: "",
     type: "input",
   },
   {
+    name: "audience",
     title: "Audience",
-    defaultValue: "Placeholder",
+    defaultValue: "",
     type: "input",
   },
 ];
 type ModalProps = {
   isOpen: boolean;
   onClose: () => void;
+  initialData: InitialModalData;
+  onSaveData: (data: InitialModalData) => void;
 };
-export const ConfigurationModal = ({ isOpen, onClose }: ModalProps) => {
+export const ConfigurationModal = ({
+  isOpen,
+  onClose,
+  initialData,
+  onSaveData,
+}: ModalProps) => {
+  const SERVER_URLS = {
+    auth0: "https://sample.auth0.com/.well-known/openid-configuration",
+    google: "https://accounts.google.com/.well-known/openid-configuration",
+    custom: "",
+  };
+  const [formValues, setFormValues] = useState<Record<string, string>>(() =>
+    Object.fromEntries(
+      MODAL_OPTIONS.map((opt) => [
+        opt.name,
+        initialData[opt.name] ?? opt.defaultValue,
+      ]),
+    ),
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    const { value, name } = e.target;
+    setFormValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  const handleServerChange = async (
+    e: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    const { value } = e.target;
+    if (!value || value === "custom") {
+      setFormValues((prev) => {
+        return {
+          ...prev,
+          authEndpoint: "",
+          tokenEndpoint: "",
+          domain: "",
+        };
+      });
+      return;
+    }
+    try {
+      setIsLoading(true);
+      const queryString = new URLSearchParams({
+        url: SERVER_URLS[value],
+      }).toString();
+      const response = await fetch(`api/discover?${queryString}`);
+      setIsLoading(false);
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(
+          errorData.message || "Couldn't fetch the server data. Try again",
+        );
+      }
+      const data = await response.json();
+      setFormValues((prev) => {
+        return {
+          ...prev,
+          authEndpoint: data.authorization_endpoint,
+          tokenEndpoint: data.token_endpoint,
+          domain: value !== "auth0" ? SERVER_URLS[value] : "samples.auth0.com",
+        };
+      });
+    } catch (error) {
+      setFormValues((prev) => {
+        return {
+          ...prev,
+          authEndpoint: "",
+          tokenEndpoint: "",
+        };
+      });
+    }
+  };
   if (!isOpen) return null;
   return (
     <div className={styles.backdrop} onClick={onClose}>
@@ -71,14 +184,47 @@ export const ConfigurationModal = ({ isOpen, onClose }: ModalProps) => {
           </button>
         </div>
         <div className={styles.content}>
-          {MODAL_OPTIONS.map((modalOption, idx) => {
+          {MODAL_OPTIONS.map((modalOption) => {
+            const { name, title, type, readOnly } = modalOption;
+            let updatedTitle = title;
+            let updatedReadOnly = readOnly;
+            if (name === "domain" && formValues["serverTemplate"] !== "auth0") {
+              updatedTitle = "Discovery Document URL";
+              updatedReadOnly = true;
+            }
+            if (formValues["serverTemplate"] === "custom") {
+              updatedReadOnly = false;
+            }
             return (
-              <div className={styles.input_container} key={idx}>
-                <label>{modalOption.title}</label>
-                <input
-                  defaultValue={modalOption.defaultValue ?? ""}
-                  className={styles.input}
-                />
+              <div className={styles.input_container} key={name}>
+                <label htmlFor={name}>{updatedTitle}</label>
+                {type === "input" ? (
+                  <input
+                    readOnly={updatedReadOnly}
+                    id={name}
+                    name={name}
+                    value={formValues[name] ?? ""}
+                    className={styles.input}
+                    onChange={handleInputChange}
+                  />
+                ) : (
+                  <select
+                    onChange={(e) => {
+                      handleServerChange(e);
+                      handleInputChange(e);
+                    }}
+                    id={name}
+                    name={name}
+                    disabled={isLoading}
+                    value={formValues[name]}
+                  >
+                    {modalOption.options?.map((option) => (
+                      <option key={option.value} value={option.value}>
+                        {option.label}
+                      </option>
+                    ))}
+                  </select>
+                )}
               </div>
             );
           })}
@@ -105,7 +251,14 @@ export const ConfigurationModal = ({ isOpen, onClose }: ModalProps) => {
             </div>
           </div>
           <div className={styles.button_container}>
-            <Button label="Save" showIcon={false} />
+            <Button
+              label="Save"
+              showIcon={false}
+              onClick={() => {
+                onSaveData(formValues as InitialModalData);
+                onClose();
+              }}
+            />
           </div>
         </div>
       </div>
