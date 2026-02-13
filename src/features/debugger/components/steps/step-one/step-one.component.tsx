d03ae3b55700ccfa68abb682eba5bd3e0c81e918
Javier Tinoco <213990346+javiert-okta@users.noreply.github.com>
2026-02-13 16:42:59 -0500

    add prop to open modal

--- src/features/debugger/components/steps/step-one/step-one.component.tsx
+++ src/features/debugger/components/steps/step-one/step-one.component.tsx
@@ -2,12 +2,12 @@ import { LinkButton } from "features/common/components/button/button.component";
 import { Codeblock, RequestData } from "../../codeblock/codeblock.component";
 import { getCompleteUrlFromRequestData } from "../utils";
 
-export const StepOne = ({ requestData }: { requestData: RequestData }) => {
+export const StepOne = ({ requestData, openModal }: { requestData: RequestData, openModal: () => void }) => {
   const urlFromRequestData = getCompleteUrlFromRequestData(requestData);
 
   return (
     <>
-      <Codeblock title="Request" type="request" requestData={requestData} />
+      <Codeblock title="Request" type="request" requestData={requestData} onClickHandler={openModal}/>
       <LinkButton label="Start" href={urlFromRequestData} />
     </>
   );
