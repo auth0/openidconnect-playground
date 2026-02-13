51c5be80025ea7b761d4e30125db52a7066d40a0
Javier Tinoco <213990346+javiert-okta@users.noreply.github.com>
2026-02-13 16:39:32 -0500

    move toolbar inside debugger steps component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import { HeroJumbotronComponent } from "features/common/components/hero-jumbotron/hero-jumbotron.component";
 import { DebuggerSteps } from "features/debugger/components/steps/debugger-steps.component";
-import { DebuggerToolbar } from "features/debugger/components/toolbar/debugger-toolbar.component";
 
 export default function Page() {
+ 
   return (
     <>
       <HeroJumbotronComponent />
-      <DebuggerToolbar />
       <DebuggerSteps />
     </>
   );
